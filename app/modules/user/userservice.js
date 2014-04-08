617837b5415e1f128c09d076285f6834017d3d0e
Maestro501 <chldbwls0114@hotmail.com>
2014-04-07 18:48:31 -0700

    userService.login should make query for user,pw

--- app/modules/user/userservice.js
+++ app/modules/user/userservice.js
@@ -1,12 +1,25 @@
 'use strict';
 
 angular.module('newsyApp.services.user', [])
-  .factory('userService', ['$location', '$rootScope',
-    function($location, $rootScope) {
+  .factory('userService', ['$location', '$rootScope', '$cookieStore', '$http',
+    function($location, $rootScope, $cookieStore, $http) {
+      
+    var accessLevels = routingConfig.accessLevels,
+        userRoles = routingConfig.userRoles,
+        currentUser = $cookieStore.get('user');
+
+    $rootScope.accessLevels = accessLevels;
+    $rootScope.userRoles = userRoles;
+
       return {
         login: function(email, pass, redirect, callback) {
           //send login request to server
+            $http.get(redirect, email).success(function(email){
+              $rootScope.user = email;
+              success(email);
+            }).error(function(){console.log('ef')});
         },
+
         logout: function(redirectPath) {
           //send logout request to server
           if(redirectPath) {
@@ -15,6 +28,10 @@ angular.module('newsyApp.services.user', [])
         },
         createAccount: function(name, email, pass, callback) {
       
-        }
+        },
+
+        accessLevels: accessLevels,
+        userRoles: userRoles,
+        user: currentUser
     }
   }])
--- backend/server.js
+++ backend/server.js
@@ -9,31 +9,57 @@ app.configure(function() {
   app.use(express.bodyParser());
 
   app.use(express.static(path.join(__dirname, '../app')));
+
+  app.get('/', function(req, res){
+    console.log('inside auth');
+      var role = routingConfig.userRoles.public,
+          username = '';
+
+      if(req.user) {
+          role = req.user.role;
+          username = req.user.username;
+      }
+
+      res.cookie('user', JSON.stringify({
+          'username': username,
+          'role': role
+      }));
+
+      res.render('index');
+  });
+  
+  app.get('/login', function(req, res){
+    console.log('inside auth');
+    res.send('loginresponse');
+      var role = routingConfig.userRoles.public,
+          username = '';
+
+      if(req.user) {
+          role = req.user.role;
+          username = req.user.username;
+      }
+
+      res.cookie('user', JSON.stringify({
+          'username': username,
+          'role': role
+      }));
+
+      res.render('index');
+  });
+
+  app.use(express.cookieParser());
+  app.use(express.cookieSession({
+    secret: 'hello'
+  }));
+
+
   app.listen(8080);
   console.log('Listening on port 8080');
 });
 
-app.get('/', function(req, res){
-  console.log('inside auth');
-    var role = routingConfig.userRoles.public,
-        username = '';
+// app.get('/newestheadlines', handler.newestHeadlinesGet);
+// // app.options('/newestheadlines', handler.newestHeadOptions);
 
-    if(req.user) {
-        role = req.user.role;
-        username = req.user.username;
-    }
-
-    res.cookie('user', JSON.stringify({
-        'username': username,
-        'role': role
-    }));
-
-    res.render('index');
-});
-
-app.get('/newestheadlines', handler.newestHeadlinesGet);
-// app.options('/newestheadlines', handler.newestHeadOptions);
-
-app.post('/article', handler.articlePost);
-app.get('/articleGet', handler.articleGet);
+// app.post('/article', handler.articlePost);
+// app.get('/articleGet', handler.articleGet);
 // app.options('/article', handler.articleOptions);
