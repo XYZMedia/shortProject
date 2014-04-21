c14166522976fcc48c3789ca5e7e9cc6677fd90a
Maestro501 <chldbwls0114@hotmail.com>
2014-04-21 13:49:59 -0700

    fixes onearticlecontroller . diffstring()

--- app/modules/articles/onearticlecontroller.js
+++ app/modules/articles/onearticlecontroller.js
@@ -148,47 +148,46 @@ $scope.hashtags = "#obama1";
         n = n.replace(/"/g, "&quot;");
 
         return n;
-    };
+    }
 
-    function diffString( oldString, newString ) {
-      var o = oldString.replace(/\s+$/, ''),
-          n = newString.replace(/\s+$/, ''),
-      i;
+    function diffString( o, n ) {
+      o = o.replace(/\s+$/, '');
+      n = n.replace(/\s+$/, '');
 
-      var out = diff(o === "" ? [] : o.split(/\s+/), n === "" ? [] : n.split(/\s+/) );
+      var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/) );
       var str = "";
 
       var oSpace = o.match(/\s+/g);
-      if (oSpace === null) {
+      if (oSpace == null) {
         oSpace = ["\n"];
       } else {
         oSpace.push("\n");
       }
       var nSpace = n.match(/\s+/g);
-      if (nSpace === null) {
+      if (nSpace == null) {
         nSpace = ["\n"];
       } else {
         nSpace.push("\n");
       }
 
-      if (out.n.length === 0) {
-          for (i = 0; i < out.o.length; i++) {
+      if (out.n.length == 0) {
+          for (var i = 0; i < out.o.length; i++) {
             str += '<del>' + escape(out.o[i]) + oSpace[i] + "</del>";
           }
       } else {
-        if (out.n[0].text === null) {
-          for (n = 0; n < out.o.length && out.o[n].text === null; n++) {
+        if (out.n[0].text == null) {
+          for (n = 0; n < out.o.length && out.o[n].text == null; n++) {
             str += '<del>' + escape(out.o[n]) + oSpace[n] + "</del>";
           }
         }
 
-        for (i = 0; i < out.n.length; i++ ) {
-          if (out.n[i].text === null) {
+        for ( var i = 0; i < out.n.length; i++ ) {
+          if (out.n[i].text == null) {
             str += '<ins>' + escape(out.n[i]) + nSpace[i] + "</ins>";
           } else {
             var pre = "";
 
-            for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text === null; n++ ) {
+            for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text == null; n++ ) {
               pre += '<del>' + escape(out.o[n]) + oSpace[n] + "</del>";
             }
             str += " " + out.n[i].text + nSpace[i] + pre;
@@ -241,5 +240,4 @@ $scope.hashtags = "#obama1";
       return { o: o, n: n };
     };
   }]);
-
 }());
